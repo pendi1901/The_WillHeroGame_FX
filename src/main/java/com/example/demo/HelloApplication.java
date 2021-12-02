a95aefbc0b549fe5c9c842da502f3a7e06f9217c
Aaryan-R-S <aaryan20004@iiitd.ac.in>
2021-12-02 20:00:55 +0530

    Created Home Screen Satic GUI

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,13 +14,17 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 680, 380);
+
+        stage.setTitle("Will Hero");
+        stage.getIcons().add(new Image("file:///D:/My Files/ARS@IIITD/Academics/Year 2/Sem3/AP/AP Project/Will Hero/refer/Images/boss orc.png"));
+        stage.setResizable(false);
+
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
